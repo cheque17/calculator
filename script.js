22d38c69a7bec5035ed6355c9ba73eaba13bb66d
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-04-03 16:05:03 -0600

    Edit index.html, script.js and styles.css

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+/*
+window.addEventListener('keydown', function (e) {
+    console.log(e.key);
+})
 
-let firstNumber;
-let operator;
-let secondNumber;
+let numberButtons = document.querySelectorAll('.button-number');
+numberButtons.forEach( button => button.addEventListener('click', (number) =>{
+    console.log(number.data-value);
+}))
+*/
+
+let firstNumber='';
+let operator='';
+let secondNumber='';
 
 
 function add(a,b) {
@@ -21,5 +31,13 @@ function divide(a,b) {
 }
 
 function operate(firstOperand, sign, secondOperand) {
-
+    if (sign == '+') {
+        return add(firstOperand, secondOperand);
+    } else if ( sign == '-'){
+        return substract(firstOperand, secondOperand);
+    } else if (sign == '*') {
+        return multiply(firstOperand, secondOperand);
+    } else if (sign == '/') {
+        return divide(firstOperand, secondOperand);
+    }
 }
