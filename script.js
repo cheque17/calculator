31fde7e41cad7dc6b84c720b5ddd5137be7cbda9
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-04-08 16:10:07 -0600

    Edit script.js

--- script.js
+++ script.js
@@ -24,99 +24,123 @@ const workScreen = document.querySelector('#work-screen');
 
 //Mathematical operations and operation selector function
 function add(a,b) {
-    return parseFloat(a) + parseFloat(b);
+	return parseFloat(a) + parseFloat(b);
 }
 
 function substract(a,b) {
-    return a - b;
+	return a - b;
 }
 
 function multiply(a,b) {
-    return a * b;
+	return a * b;
 }
 
 function divide(a,b) {
-    return a / b;
+	return a / b;
 }
 
 
 function operate(firstOperand, sign, secondOperand) {
-    if (sign == '+') {
-        return add(firstOperand, secondOperand);
-    } else if ( sign == '-'){
-        return substract(firstOperand, secondOperand);
-    } else if (sign == '*') {
-        return multiply(firstOperand, secondOperand);
-    } else if (sign == '/') {
-        return divide(firstOperand, secondOperand);
-    }
+	if (sign == '+') {
+		return add(firstOperand, secondOperand);
+	} else if ( sign == '-'){
+		return substract(firstOperand, secondOperand);
+	} else if (sign == '*') {
+		return multiply(firstOperand, secondOperand);
+	} else if (sign == '/') {
+		return divide(firstOperand, secondOperand);
+	}
 }
 
 
 function showOnScreen(screenToBeUsed, valueDisplayed){
-    screenToBeUsed.textContent = valueDisplayed;
+	screenToBeUsed.textContent = valueDisplayed;
 }
 
 
 //Event listeners
 numberButtons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-        if (inputDisplayValue == '0') {
-            inputDisplayValue = event.target.textContent;
-        } else if (inputDisplayValue != '0') {
-            inputDisplayValue += event.target.textContent
-        }
-        equalButtonCounter = 0;
-        showOnScreen(userInputScreen, inputDisplayValue);        
-    });
+	button.addEventListener('click', (event) => {
+		if (inputDisplayValue === '0') {
+			inputDisplayValue = event.target.textContent;
+		} else if (inputDisplayValue != '0') {
+			inputDisplayValue += event.target.textContent
+		}
+		equalButtonCounter = 0;
+		showOnScreen(userInputScreen, inputDisplayValue);        
+	});
 })
 
 operatorButtons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-        equalButtonCounter += 1;
-        firstNumber = inputDisplayValue;
-        operator = event.target.textContent;
-        calcWorkingValue = inputDisplayValue + ' ' + event.target.textContent;
-        showOnScreen(workScreen, calcWorkingValue);
-        inputDisplayValue = '0';
-        showOnScreen(userInputScreen, inputDisplayValue);
-    })
+	button.addEventListener('click', (event) => {			
+		if (equalButtonCounter) {
+			firstNumber = inputDisplayValue;
+			operator = event.target.textContent;
+			calcWorkingValue= inputDisplayValue + ' ' + event.target.textContent;
+			showOnScreen(workScreen, calcWorkingValue);
+			inputDisplayValue='0';
+			secondNumber='';
+			showOnScreen(userInputScreen, inputDisplayValue); 
+		} else if (firstNumber) {
+			equalButtonCounter += 1;
+			secondNumber=inputDisplayValue;
+			inputDisplayValue= operate(firstNumber, operator, secondNumber);
+			calcWorkingValue = inputDisplayValue + ' ' + event.target.textContent; 
+			showOnScreen(workScreen, calcWorkingValue);		
+			operator = event.target.textContent;
+			firstNumber= inputDisplayValue
+			inputDisplayValue='0';
+			showOnScreen(userInputScreen, inputDisplayValue); 
+		} else {
+			equalButtonCounter += 1;
+			firstNumber = inputDisplayValue;
+			operator = event.target.textContent;
+			calcWorkingValue = inputDisplayValue + ' ' + event.target.textContent;
+			showOnScreen(workScreen, calcWorkingValue);
+			inputDisplayValue = '0';
+			showOnScreen(userInputScreen, inputDisplayValue);
+		}
+	     
+	})
 })
 
 equalButton.addEventListener('click', (button) => {
-    if (!firstNumber || equalButtonCounter) {
-        return
-    } 
-    equalButtonCounter +=1;
-    secondNumber=inputDisplayValue;
-    calcWorkingValue += ' ' + inputDisplayValue; 
-    showOnScreen(workScreen, calcWorkingValue);
-    inputDisplayValue= operate(firstNumber, operator, secondNumber);
-    showOnScreen(userInputScreen, inputDisplayValue);        
+	if (!firstNumber || equalButtonCounter) {
+		return
+	} 
+	equalButtonCounter +=1;
+	secondNumber=inputDisplayValue;
+	calcWorkingValue += ' ' + inputDisplayValue; 
+	showOnScreen(workScreen, calcWorkingValue);
+	inputDisplayValue= operate(firstNumber, operator, secondNumber);
+	showOnScreen(userInputScreen, inputDisplayValue);        
 })
 
 
 dotButton.addEventListener('click', (button) => {
-    inputDisplayValue += '.';
-    showOnScreen(userInputScreen, inputDisplayValue);    
+	inputDisplayValue += '.';
+	showOnScreen(userInputScreen, inputDisplayValue);    
 })
 
 clearButton.addEventListener('click', (button) => {
-    equalButtonCounter=0;
-    firstNumber='';
-    operator='';
-    secondNumber='';
-    inputDisplayValue='0';
-    showOnScreen(userInputScreen, inputDisplayValue);
-    calcWorkingValue='';
-    showOnScreen(workScreen, calcWorkingValue);
+	equalButtonCounter=0;
+	firstNumber='';
+	operator='';
+	secondNumber='';
+	inputDisplayValue='0';
+	showOnScreen(userInputScreen, inputDisplayValue);
+	calcWorkingValue='';
+	showOnScreen(workScreen, calcWorkingValue);
 })
 
 
 
 deleteButton.addEventListener('click', button => {
-    let wordLength = inputDisplayValue.length;
-    inputDisplayValue = inputDisplayValue.substring(0, (wordLength-1))
-    showOnScreen(userInputScreen, inputDisplayValue);
+	let wordLength = inputDisplayValue.length;
+	inputDisplayValue = inputDisplayValue.substring(0, (wordLength-1))
+	showOnScreen(userInputScreen, inputDisplayValue);
 })
+
+
+
 
